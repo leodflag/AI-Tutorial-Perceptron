9fd5c595a3fb2affdb4d464a2e97bc5e08a74550
leodflag <lovedoglion5@gmail.com>
2020-08-24 23:17:22 +0800

    Add files via upload

--- python/perceptron.py
+++ python/perceptron.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu May 10 15:27:35 2018
-
 @author: lovedoglion
 """
 
@@ -14,7 +13,7 @@ b1=0
 
 #neural(input1,input2,desire output)
 def neural(x1, x2, d):
-    global w1,w2,b1 #在函数内部访问并修改全局变量
+    global w1,w2,b1 #在函數内部訪問並修改全局變量
     net=w1*x1+w2*x2+b1
     Y=1/(1+ math.exp(-net))#Sigmoid函數
     #backward path 更新權重的方法
@@ -44,7 +43,7 @@ def neuralXOR():
     z=x^y #XOR位元運算
     return print('neuralXOR：',neural(x,y,z))
     
-#初始化權重與
+#初始化權重
 def init():
     global w1,w2,b1
     w1=random.random()
@@ -53,4 +52,6 @@ def init():
     
 #main
 for j in range(1000):
-    neuralOR()
+    #neuralAND()
+    #neuralOR()
+    neuralXOR() # 單顆神經元無法解決XOR問題
